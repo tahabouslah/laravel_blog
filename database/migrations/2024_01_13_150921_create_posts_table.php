b233d76412208ce91200e6c029c9f89c75eecc83
Bouslah Taha <bouslahtaha33@gmail.com>
2024-01-17 23:05:55 +1300

    create a post model

--- database/migrations/2024_01_13_150921_create_posts_table.php
+++ database/migrations/2024_01_13_150921_create_posts_table.php
@@ -11,15 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('username');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean("isAdmin")->default(false);
-            $table->rememberToken();
+            $table->string('title');
+            $table->text('excerpt');
+            $table->text('body');
             $table->timestamps();
+            $table->timestamp('published_at')->nullable();
         });
     }
 
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
