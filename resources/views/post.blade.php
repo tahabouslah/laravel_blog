68c2cd021d803342748e2224c902cfe9065e9841
Bouslah Taha <bouslahtaha33@gmail.com>
2024-01-17 23:06:30 +1300

    create a view for posts and a route

---
+++ resources/views/post.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>post</title>
+    <link rel="stylesheet" href="/main.css">
+</head>
+<body>
+    <article>
+        <h1><?= $post->title ?><h1>
+        <p><?= $post->body  ?></p>
+    </article>
+
+    <a href="/">Go Back </a>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $posts = cache()->remember("posts", 5, function () {
+        return  Post::all();
+    });
+    return view('posts', [
+        'posts' => $posts
+    ]);
 });
+
+
+Route::get('/post/{post}', function ($id) {
+
+    //find a post bby its slug/id and pass it to a view called "post"
+
+
+
+    $post = cache()->remember("post.{$id}", 5, function () use ($id) {
+        return Post::find($id);
+    });
+
+    return view("post", ['post' => $post]);
+})->whereNumber("post");
