6b41e0f906a7f33cae64d6d1697328d4e5bc2762
Bouslah Taha <bouslahtaha33@gmail.com>
2024-01-19 11:30:24 +1300

    relationship between User and Post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+use \App\Models\User;
+use \App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +16,57 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        Category::truncate();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user = User::factory()->create();
+
+        $pers = Category::create([
+            'name' => 'personal',
+            'slug' => 'personal'
+        ]);
+        $wrk = Category::create([
+            'name' => 'work',
+            'slug' => 'work'
+        ]);
+        $hob = Category::create([
+            'name' => 'hobbies',
+            'slug' => 'hobbies'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $pers->id,
+            'title' => 'My family Post',
+            'slug' => 'my-first-post',
+            'excerpt' => 'lorem ipsum dolar sit amet.',
+            'body' => 'lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.'
+        ]);
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $pers->id,
+            'title' => 'My family Post',
+            'slug' => 'my-first-post',
+            'excerpt' => 'lorem ipsum dolar sit amet.',
+            'body' => 'lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $wrk->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-second-post',
+            'excerpt' => 'lorem ipsum dolar sit amet.',
+            'body' => 'lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.'
+        ]);
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $hob->id,
+            'title' => 'My Hobbie Post',
+            'slug' => 'my-third-post',
+            'excerpt' => 'lorem ipsum dolar sit amet.',
+            'body' => 'lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.lorem ipsum dolar sit amet.'
+        ]);
+
     }
 }
